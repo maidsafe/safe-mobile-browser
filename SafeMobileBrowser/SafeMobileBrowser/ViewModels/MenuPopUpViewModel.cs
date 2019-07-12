2357f4eb845fccdb92b9ebec8a44fde284221300
vigneshwara044 <vigneshwara044@gmail.com>
2019-07-12 14:40:04 +0530

    fix/browser: close popup page before opening modal page and prevent opacity change while closing popup page

--- SafeMobileBrowser/SafeMobileBrowser/ViewModels/MenuPopUpViewModel.cs
+++ SafeMobileBrowser/SafeMobileBrowser/ViewModels/MenuPopUpViewModel.cs
@@ -68,6 +68,7 @@ namespace SafeMobileBrowser.ViewModels
                 SetProperty(ref _selectedPopMenuItem, value);
                 if (value != null)
                 {
+                    Navigation.PopPopupAsync();
                     OnPopupMenuSelection();
                 }
             }
@@ -286,9 +287,6 @@ namespace SafeMobileBrowser.ViewModels
                     MessageCenterConstants.DisplayAlertMessage,
                     ex.Message);
             }
-
-            // TODO: delay in popup. Needs refactoring
-            await Navigation.PopPopupAsync();
             SelectedPopMenuItem = null;
         }
     }
