03c107633e544f015e0fb240653a1ec1b4b8be82
Ravinder Jangra <ravinderjangra@live.com>
2019-08-12 16:54:43 +0530

    chore(delete log): change toast internal to 1.5 sec and don't use clear on iOS

--- SafeMobileBrowser/SafeMobileBrowser/ViewModels/LogsModalPageViewModel.cs
+++ SafeMobileBrowser/SafeMobileBrowser/ViewModels/LogsModalPageViewModel.cs
@@ -14,6 +14,7 @@ namespace SafeMobileBrowser.ViewModels
     public class LogsModalPageViewModel : BaseViewModel
     {
         private readonly string _logFilesPath = DependencyService.Get<IPlatformService>().ConfigFilesPath;
+        private readonly TimeSpan _toastTimeSpan = TimeSpan.FromSeconds(1.5);
         private readonly string _logFileExtension = "log";
         private readonly INavigation _navigation;
 
@@ -68,12 +69,12 @@ namespace SafeMobileBrowser.ViewModels
                 var logFileToDelete = Path.Combine(_logFilesPath, $"{fileName}.{_logFileExtension}");
                 File.Delete(logFileToDelete);
                 LogFiles.Remove(fileName);
-                UserDialogs.Instance.Toast(Constants.LogFileDeleteSuccessfully);
+                UserDialogs.Instance.Toast(Constants.LogFileDeleteSuccessfully, _toastTimeSpan);
             }
             catch (Exception ex)
             {
                 Logger.Error(ex);
-                UserDialogs.Instance.Toast(ErrorConstants.FailedToDeleteLogFile);
+                UserDialogs.Instance.Toast(ErrorConstants.FailedToDeleteLogFile, _toastTimeSpan);
             }
         }
 
@@ -87,12 +88,12 @@ namespace SafeMobileBrowser.ViewModels
                 {
                     await Clipboard.SetTextAsync(logFileText);
                 });
-                UserDialogs.Instance.Toast(Constants.LogFileContentReadSuccessfully);
+                UserDialogs.Instance.Toast(Constants.LogFileContentReadSuccessfully, _toastTimeSpan);
             }
             catch (Exception ex)
             {
                 Logger.Error(ex);
-                UserDialogs.Instance.Toast(ErrorConstants.FailedToCopyLogFileContent);
+                UserDialogs.Instance.Toast(ErrorConstants.FailedToCopyLogFileContent, _toastTimeSpan);
             }
         }
 
@@ -113,26 +114,42 @@ namespace SafeMobileBrowser.ViewModels
                 var lastModifiedFile = directory.GetFiles("*.log")
                     .OrderByDescending(f => f.LastWriteTime)
                     .FirstOrDefault()
-                    ?.Name;
+                    ?.Name
+                    .Replace(".log", string.Empty);
 
                 foreach (var file in LogFiles)
                 {
-                    if (file == lastModifiedFile)
-                        continue;
-
-                    var logFileToDelete = Path.Combine(_logFilesPath, $"{file}.{_logFileExtension}");
-                    File.Delete(logFileToDelete);
+                    if (file != lastModifiedFile)
+                    {
+                        var logFileToDelete = Path.Combine(_logFilesPath, $"{file}.{_logFileExtension}");
+                        File.Delete(logFileToDelete);
+                    }
                 }
 
-                LogFiles.Clear();
-                LogFiles.Add(lastModifiedFile);
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    // Known issue: clear list by removing item(s) one by one on iOS
+                    if (Device.RuntimePlatform == Device.Android)
+                    {
+                        LogFiles.Clear();
+                    }
+                    else
+                    {
+                        while (LogFiles.Count > 0)
+                        {
+                            LogFiles.RemoveAt(0);
+                        }
+                    }
 
-                UserDialogs.Instance.Toast(Constants.LogFileDeleteSuccessfully);
+                    LogFiles.Add(lastModifiedFile);
+                });
+
+                UserDialogs.Instance.Toast(Constants.LogFileDeleteSuccessfully, _toastTimeSpan);
             }
             catch (Exception ex)
             {
                 Logger.Error(ex);
-                UserDialogs.Instance.Toast(ErrorConstants.FailedToDeleteLogFile);
+                UserDialogs.Instance.Toast(ErrorConstants.FailedToDeleteLogFile, _toastTimeSpan);
             }
         }
 
