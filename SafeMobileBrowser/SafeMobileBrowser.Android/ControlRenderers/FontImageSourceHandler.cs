77b47350fa3d82af42dec52c604ed34579b724b2
Vigneshwara Prakash <vigneshwara044@gmail.com>
2019-08-14 14:31:00 +0530

    feat(Dark mode): add dark mode (#158)
    * feat/browser: adding dark mode theme, and moving the colors from App.xaml to respective themes, adding theme helper. Updating the welcome page WIP and colours need to be updated and toggle theme is called on opening popup menu
    
    * feat/browser: update welcome page on theme change & restore saved theme on open
    
    * update safe label color, settings page text color on theme changhe, load correct theme welcomepage on opening application
    
    * homepage icons only work with labels
    
    * add fontimagesource handler
    
    * refactor/browser: update colours and remove commented code
    
    * feat/browser: update status bar color and refactor code
    
    * fix/browser: update welcome page text on theme change and minor refactoring
    
    * refactor/browser: add update colors
    
    * refactor/browser: update color names and box view color on theme change
    
    * chore(refactor): change statusbarmanager to nativethememanager
    
    * style/browser: make shadow visible only on light theme and update modal page background color
    
    * style/browser: update alert dialog dynamically in android and update android status bar color using themes
    
    * status bar color changes
    
    * style/browser: update status bar color consistently in dark mode
    
    * refactor/browser: pass if app launched new as parameter to native theme changer instead of using preferences
    
    * refactor/browser: rename 'onOpened' parameter to 'isAppLaunched'
    
    * refactor/browser: remove unused code from index.html
    
    * fix/browser: update theme on error page
    
    * refactor/browser: update app theme preference to bool and preference key to constants
    
    * refactor/browser: move label attributes to styles
    
    * refactor/browser: refactor native theme manager code
    
    * refactor/browser: refactoring native theme manager
    
    * refactor/browser: refactor the styling for icon
    
    * fix/browser: fix primary and secondary icon color

---
+++ SafeMobileBrowser/SafeMobileBrowser.Android/ControlRenderers/FontImageSourceHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Graphics;
+using Android.Util;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Xamarin.Forms.Color;
+
+[assembly: ExportImageSourceHandler(typeof(FontImageSource), typeof(SafeMobileBrowser.Droid.ControlRenderers.FontImageSourceHandler))]
+
+namespace SafeMobileBrowser.Droid.ControlRenderers
+{
+    public sealed class FontImageSourceHandler : IImageSourceHandler
+    {
+        private static Dictionary<string, Typeface> typefaceCache = new Dictionary<string, Typeface>();
+
+        public Task<Bitmap> LoadImageAsync(
+            ImageSource imagesource,
+            Context context,
+            CancellationToken cancelationToken = default(CancellationToken))
+        {
+            Bitmap image = null;
+            var fontsource = imagesource as FontImageSource;
+            if (fontsource != null)
+            {
+                var paint = new Paint
+                {
+                    TextSize = TypedValue.ApplyDimension(ComplexUnitType.Dip, (float)fontsource.Size, context.Resources.DisplayMetrics),
+                    Color = (fontsource.Color != Color.Default ? fontsource.Color : Color.White).ToAndroid(),
+                    TextAlign = Paint.Align.Left,
+                    AntiAlias = true
+                };
+
+                if (!typefaceCache.TryGetValue(fontsource.FontFamily, out Typeface typeface))
+                {
+                    typeface = fontsource.FontFamily.ToTypeFace();
+                    typefaceCache.Add(fontsource.FontFamily, typeface);
+                }
+                paint.SetTypeface(typeface);
+
+                var width = (int)(paint.MeasureText(fontsource.Glyph) + .5f);
+                var baseline = (int)(-paint.Ascent() + .5f);
+                var height = (int)(baseline + paint.Descent() + .5f);
+                image = Bitmap.CreateBitmap(width, height, Bitmap.Config.Argb8888);
+                var canvas = new Canvas(image);
+                canvas.DrawText(fontsource.Glyph, 0, baseline, paint);
+            }
+
+            return Task.FromResult(image);
+        }
+    }
+}
